7ddc8b389dc5d997f42cfdddd27fa5cda0ed3868
John Varghese <126874846+John-Varghese-EH@users.noreply.github.com>
2025-05-09 12:56:13 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,1 +1,24 @@
+from setuptools import setup, find_packages
 
+setup(
+    name="ShadowMask",
+    version="1.0.0",
+    description="A privacy tool to protect images from AI facial recognition.",
+    author="John Varghese",
+    author_email="your.email@example.com",
+    packages=find_packages(where="src"),
+    package_dir={"": "src"},
+    install_requires=[
+        "kivy",
+        "pillow",
+        "numpy",
+        "torch",
+        "torchvision"
+    ],
+    entry_points={
+        "console_scripts": [
+            "shadowmask-gui=shadowmask.gui:ShadowMaskApp.run",
+        ]
+    },
+    python_requires=">=3.8",
+)
